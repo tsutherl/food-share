f86dc3ef1091b0b97a5f6504f7d3102bac58f2c6
Tatiana Sutherland <tsutherl@oberlin.edu>
2016-11-02 18:33:25 -0400

    implements logout

--- app/components/navbar/NavbarContainer.jsx
+++ app/components/navbar/NavbarContainer.jsx
@@ -1,7 +1,8 @@
 import {connect} from 'react-redux'
 import Navbar from './Navbar'
+import {logout} from 'APP/app/reducers/auth'
 
 export default connect(
     ({auth}) => ({auth}),
-     null)
+    {logout})
 (Navbar)
