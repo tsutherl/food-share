8003eb087d7a1d3cdcfac3a710ffc358a5705568
Cara Weber <caraweber@hotmail.com>
2016-11-04 12:51:50 -0400

    started tweaking cart view

--- app/components/cart/Cart.jsx
+++ app/components/cart/Cart.jsx
@@ -4,8 +4,9 @@ export default class Cart extends Component {
 
   componentDidMount() {
     const user = this.props.user;
-    console.log("THE PROPS ON CART", this.props)
-    user && this.props.onLoadItems(user.id);
+    let ready = (user && this.props.items);
+    ready && this.props.onLoadItems(user.id);
+    
 
   }
 
@@ -17,7 +18,13 @@ export default class Cart extends Component {
 
           {this.props.items &&
             this.props.items.map((item, idx) => (
-            <li className="cart-list" key={idx}><img src={item.product.picture} className="cart-pic" /> {item.product.name} <span>qty: {item.quantity}</span><button onClick = {() => this.props.deleteItem(item.id, item.user_id)}className= "btn-cart-remove"><img src ="https://cdn3.iconfinder.com/data/icons/buttons/512/Icon_12-512.png" /></button> </li>
+            <li className="cart-list" key={idx}>
+              <img src={item.product.picture} className="cart-pic" /> {item.product.name} 
+              <span>qty: {item.quantity}</span>
+              <button onClick = {() => this.props.deleteItem(item.id, item.user_id)}
+                className= "btn-cart-remove">
+                <img src ="https://cdn3.iconfinder.com/data/icons/buttons/512/Icon_12-512.png" />
+              </button> </li>
             ))
           }
 
