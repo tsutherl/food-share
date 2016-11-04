1ceca2cd833eaf5a8cfa00b051d417a384542e49
Tatiana Sutherland <tsutherl@oberlin.edu>
2016-11-04 16:41:25 -0400

    fixed and styled landing page

--- app/components/home/Home.jsx
+++ app/components/home/Home.jsx
@@ -4,16 +4,16 @@ import {Link} from 'react-router';
 export default class Home extends Component {
   render() {
     return (
-     <div> 
+     <div className="home-cover"> 
       <a name="about"></a>
         <div className="intro-header">
-            <section>
+            
             <div className="container">
 
-                <div className="row text-center">
+                <div className="row text-center vertical-center">
                     <div className="col-lg-12">
                         <div className="intro-message">
-                            <h1 className='center-block text-center'>Make A Swish</h1>
+                            <h1 className='center-block'>Make A Swish</h1>
                             <h3>And Watch It Come True</h3>
                             <hr className="intro-divider"/>
                             <ul className="list-inline intro-social-buttons">
@@ -26,7 +26,6 @@ export default class Home extends Component {
                 </div>
 
             </div>
-            </section>
         </div>
       </div>
     )
