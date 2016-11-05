a17513451941e422f2eb4dfe9f437dac87f56850
Khristian Brooks <kh.brooks02@gmail.com>
2016-11-05 17:29:37 -0400

    removed unused variables and elements from Login

--- app/components/login/Login.jsx
+++ app/components/login/Login.jsx
@@ -4,14 +4,9 @@ import {Link, browserHistory} from 'react-router'
 export default class Login extends Component {
 
 	render (){
-		const newAuth= this.props.newAuth
-		let wrongPassword= false;
+		const newAuth= this.props.newAuth;
 		return (
 			  <div>
-			  {wrongPassword? <div class="alert alert-danger alert-dismissible" role="alert">
-			    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-			    <strong>Warning!</strong> Better check yourself, youre not looking too good.
-			  </div> : ""}
 			  <form onSubmit={evt => {
 			    evt.preventDefault()
 			    this.props.login(evt.target.username.value, evt.target.password.value)
@@ -20,7 +15,6 @@ export default class Login extends Component {
 			    		browserHistory.push('/home')	
 			    	}
 			    	else{
-			    		wrongPassword=true;
 			    		alert("Wrong UserName/Password!!");
 			    	}
 			    })
