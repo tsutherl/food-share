7a2144d83f4f22e4960f54e8808bc9b88afdb7ab
Khristian Brooks <kh.brooks02@gmail.com>
2016-11-05 00:26:32 -0400

    Redirect to home when logged in/ Error message when login is incorrect

--- app/components/login/Login.jsx
+++ app/components/login/Login.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react';
+import {Link, browserHistory} from 'react-router'
+ 
+export default class Login extends Component {
 
-export default  ({ login }) => (
-  <form onSubmit={evt => {
-    evt.preventDefault()
-    login(evt.target.username.value, evt.target.password.value)
-  } }>
-    <input name="username" />
-    <input name="password" type="password" />
-    <input type="submit" value="Login" />
-  </form>
-)
+	render (){
+		const newAuth= this.props.newAuth
+		let wrongPassword= false;
+		return (
+			  <div>
+			  {wrongPassword? <div class="alert alert-danger alert-dismissible" role="alert">
+			    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			    <strong>Warning!</strong> Better check yourself, youre not looking too good.
+			  </div> : ""}
+			  <form onSubmit={evt => {
+			    evt.preventDefault()
+			    this.props.login(evt.target.username.value, evt.target.password.value)
+			    .then(function(){ 
+			    	if(newAuth().auth){
+			    		browserHistory.push('/home')	
+			    	}
+			    	else{
+			    		wrongPassword=true;
+			    		alert("Wrong UserName/Password!!");
+			    	}
+			    })
+			  } }>
+			    <input name="username" />
+			    <input name="password" type="password" />
+			    <input type="submit" value="Login" />
+			  </form>
+			  </div>
+			)
+	 }
+}
 
