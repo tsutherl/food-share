35e364e5bbe76fc3ae64db79d828d68a132b25ac
Cara Weber <caraweber@hotmail.com>
2016-11-07 16:55:23 -0500

    refactored order routes and cart display a bit

--- server/api.js
+++ server/api.js
@@ -9,6 +9,7 @@ api
   .use('/users', require('./users'))
   .use('/stickers', require('./stickers'))
   .use('/orders', require('./orders'))
+  
 
 // Send along any errors
 api.use((err, req, res, next) => {
