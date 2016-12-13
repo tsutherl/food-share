113025293bb596a28af6578b9ec326178c4ad088
Tatiana Sutherland <tsutherl@oberlin.edu>
2016-12-13 11:36:56 -0500

    initial commit

--- app/main.jsx
+++ app/main.jsx
@@ -8,26 +8,13 @@ import {whoami} from './reducers/auth'
 
 import store from './store'
 import HomeContainer from './components/home/HomeContainer'
-import StickersContainer from './components/stickers/StickersContainer'
-import StickerContainer from './components/stickers/StickerContainer'
 import UserContainer from './components/user/UserContainer'
 import LoginContainer from './components/login/LoginContainer'
 import App from './components/App'
 import WhoAmI from './components/WhoAmI'
-import CartContainer from './components/cart/CartContainer'
 import UserOrdersContainer from './components/user/UserOrdersContainer'
-import CheckoutContainer from './components/checkout/CheckoutContainer'
 
 
-
-
-const fetchCart = (nextState, _replace, done) => {
-  store.dispatch(whoami())
-    .then(auth => {
-      store.dispatch(loadItemsAsync(auth.user.id))
-  })
-    .then(()=> done())
-}
 
 
 
@@ -50,16 +37,12 @@ render (
   <Provider store={store}>
     <Router history={browserHistory}>
 
-        <Route path='/' component={App} onEnter={fetchCart}>
+        <Route path='/' component={App}>
             <IndexRedirect to='home'/>
-              <Route path='home' component={HomeContainer} onEnter={fetchCart}/>
+              <Route path='home' component={HomeContainer} />
               <Route path='login' component={LoginContainer}/>
-              <Route path='cart' component={CartContainer} onEnter={fetchCart} />
-              <Route path='checkout' component={CheckoutContainer} />
               <Route path='user' component={UserContainer}/>
               <Route path='user/orders' component={UserOrdersContainer}/>
-              <Route path='stickers' component={StickersContainer}/>
-              <Route path='stickers/:stickerId' component={StickerContainer} />
         </Route>
     </Router>
   </Provider>,
