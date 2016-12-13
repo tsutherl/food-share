1bfa9b7cc10d6729f6acdf69b1ae05f8ffd15e4b
Sami Lugar <sami.lugar@gmail.com>
2016-12-13 14:25:03 -0500

    component did mount gets locations from db

--- app/components/map/GoogleMap.jsx
+++ app/components/map/GoogleMap.jsx
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import {Link} from 'react-router';
 import store from 'APP/app/store'
 import {mapStyle} from 'APP/utils/mapstyle.js'
+import axios from 'axios';
 
 export default class GoogleMap extends Component {
   constructor(props){
     super(props)
     this.initMap = this.initMap.bind(this)
-
+    this.getLocations = this.getLocations.bind(this)
     //dummy data
     this.state = {
       user: {
@@ -19,7 +20,7 @@ export default class GoogleMap extends Component {
   }
 
   componentDidMount(){
-    initMap(this.props.user, this.props.offerings)
+    this.getLocations()
   }
 
   render() {
@@ -29,6 +30,13 @@ export default class GoogleMap extends Component {
     )
   }
 
+  // make this sockety in the future
+  getLocations(){
+    axios.get('api/offerings')
+    .then(response => {
+      this.initMap(this.props.user, response)
+    })
+  }
   initMap(user, locations){
     //sets user's map to center on their own location
     //this is tentative based on data structure of db
@@ -49,13 +57,14 @@ export default class GoogleMap extends Component {
        });
 
     // create markers based on location array
-    locations.forEach(location => {
+    locations.forEach((location, index) => {
       var latLng = new google.maps.LatLng(offering.location[0], offering.location[1])
       var options = {
         //animation,
         //icon,
-        position: latLng;
-        map: map
+        position: latLng,
+        map: map,
+        label: index+1
       }
       var marker = new google.maps.Marker(options);
     })
